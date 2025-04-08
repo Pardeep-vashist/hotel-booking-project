1023404bcec70bfd10ebc048417683bcf89e902a
Pardeep-vashist <114797978+Pardeep-vashist@users.noreply.github.com>
2025-04-08 18:09:22 +0530

    Update ALLOWED_HOSTS

--- hotel_webapp/hotel_webapp/settings.py
+++ hotel_webapp/hotel_webapp/settings.py
@@ -36,7 +36,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-ALLOWED_HOSTS = ["192.168.29.57","127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 # Application definition
 
